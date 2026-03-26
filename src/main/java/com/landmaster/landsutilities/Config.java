ee709f84328a24dfd1b2b5772ffd1870aaa2f719
Landmaster <python0429@gmail.com>
2026-03-25 21:56:23 -0400

    lang file fixes

--- src/main/java/com/landmaster/landsutilities/Config.java
+++ src/main/java/com/landmaster/landsutilities/Config.java
@@ -6,7 +6,7 @@ public class Config {
     private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
 
     public static final ModConfigSpec.IntValue MAX_LINKED_BLOCKS = BUILDER
-            .comment("The maximum number of blocks that can be linked to a remote control at base. Can be increased by enchantments.")
+            .comment("The maximum number of blocks that can be linked to a remote control at base. Can be increased by the Remote Control Capacity enchantment.")
             .defineInRange("maxLinkedBlocks", 5, 1, Integer.MAX_VALUE);
 
     static final ModConfigSpec SPEC = BUILDER.build();
