00cc651eb2160cc8564e225d91b8af8fb5cd9571
Landmaster <python0429@gmail.com>
2026-08-26 12:06:11 -0400

    fix compat with Sophisticated Storage

--- src/main/java/com/landmaster/landsutilities/mixin/sophisticatedstorage/StorageContainerMenuMixin.java
+++ src/main/java/com/landmaster/landsutilities/mixin/sophisticatedstorage/StorageContainerMenuMixin.java
@@ -1,18 +1,22 @@
 package com.landmaster.landsutilities.mixin.sophisticatedstorage;
 
 import com.landmaster.landsutilities.item.RemoteControlItem;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.player.Player;
 import net.p3pp3rf1y.sophisticatedstorage.common.gui.StorageContainerMenu;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(StorageContainerMenu.class)
 public class StorageContainerMenuMixin {
-    @Redirect(method = "stillValid", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;distanceToSqr(DDD)D"))
-    private double adjustPlayerDistance(Player instance, double x, double y, double z) {
-        var data = RemoteControlItem.MENU_TO_REMOTE_RANGE.get(instance.containerMenu);
-        var range = data == null ? 0.0 : data.range();
-        return instance.distanceToSqr(x, y, z) / Math.max(range, 1.0);
+    @ModifyArg(method = "stillValid", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;isWithinBlockInteractionRange(Lnet/minecraft/core/BlockPos;D)Z"))
+    private double adjustPlayerInteractionRange(BlockPos pos, double range, @Local(argsOnly = true, name = "player") Player player) {
+        var data = RemoteControlItem.MENU_TO_REMOTE_RANGE.get(player.containerMenu);
+        if (data != null && data.range() > 1.0f) {
+            range *= data.range();
+        }
+        return range;
     }
 }
